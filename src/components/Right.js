0b3c03edba04be47274cf99444e2f7b68e683f3a
Lee Changmoo <lcm8543@gmail.com>
2022-04-19 20:50:33 +0900

    Fix: 첫 데이터 랜더링시 마지막 데이터 문구 오류

--- src/components/Right.js
+++ src/components/Right.js
@@ -121,12 +121,11 @@ const Right = ({ data, isTeam, isRetire }) => {
         ))}
         {isLoading ? <Loading /> : null}
         <LastData>
-          {matchDatasPiece.filter(
-            (matchData) => filteringData(matchData) !== null
-          ).length === 0 // 필터링 후 전부 null값이라면
+          {matchDatas.filter((matchData) => filteringData(matchData) !== null)
+            .length === 0 // 필터링 후 전부 null값이라면
             ? "데이터가 없습니다."
             : (!isLoading && isScrollEnd) || // 로딩이 안 걸리고 스크롤이 마지막에 위치해있거나,
-              matchDatasPiece.filter(
+              matchDatas.filter(
                 (matchData) => filteringData(matchData) !== null
               ).length <= 10 // 10개 이하의 데이터만 갖고 있다면
             ? `마지막 데이터입니다.`
